3b8ba2bb21767d189db51739f80cb13322e373b9
Anuj Singh <Anuj Singh@LAPTOP-IL45AT31>
2016-08-19 17:55:45 +0530

    update

--- hyve-qa-auto/src/com/hyve/qa/ui/main/HomeTest.java
+++ hyve-qa-auto/src/com/hyve/qa/ui/main/HomeTest.java
@@ -1,10 +1,17 @@
 package com.hyve.qa.ui.main;
 
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.KeyEvent;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
@@ -12,7 +19,7 @@ import org.testng.annotations.Test;
 
 public class HomeTest {
 
-	public String hyvecare = "https://www.hyve.buzz/hyve-care";
+	public String hyvecare = "https://www.hyve.buzz/hyve-care-products";
 	public String hyvehome = "https://www.hyve.buzz/";
 	public String hyvelogin = "https://www.hyve.buzz/login";
 	public WebDriver driver = new FirefoxDriver();
@@ -36,59 +43,60 @@ public class HomeTest {
 	public void testLogin() {
 
 		// driver.get(hyvelogin);
-		driver.findElement(By.xpath("//div[2]/div[1]/div/div/div/div/div[3]/div/div/div[1]/a[2]")).click();
+		driver.findElement(By.className("logout-header")).click();
+		//xpath("//div[2]/div[1]/div/div/div/div/div[3]/div/div/div[1]/a[2]
 		driver.findElement(By.id("UserEmail")).clear();
 		driver.findElement(By.id("UserEmail")).sendKeys("anuj.singh@hyve.buzz");
 		driver.findElement(By.id("UserPassword")).clear();
 		driver.findElement(By.id("UserPassword")).sendKeys("1234567");
 		driver.findElement(By.xpath("//div[2]/div[2]/div/div/div/div/div[2]/div/div/form/div[3]/button")).submit();
-
 		String expected = "Anuj";
 		String actualtext = driver.findElement(By.className("username_name")).getText();
-		// Thread.sleep(3000);
+		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+		//Thread.sleep(3000);
 		Assert.assertEquals(actualtext, expected);
 
 	}
 
-	// TODO: Correct below test method.
-	/*
-	 * @Test public void Hyve_Care() throws AWTException, InterruptedException{
-	 * driver.get(hyvecare);
-	 * driver.findElement(By.xpath("//div[3]/div[3]/div/ul/li[2]/a")).click();
-	 * //Enter Values in Text Field
-	 * driver.findElement(By.id("BugName")).sendKeys("Anuj Singh");
-	 * driver.findElement(By.id("BugEmail")).sendKeys("anuj@gmail.com");
-	 * //Select Device from Drop down. Select drpproductid= new Select
-	 * (driver.findElement(By.name("data[Bug][product_id]")));
-	 * drpproductid.selectByVisibleText("Buzz BU01"); //Select Product Category
-	 * from Drop down. Select drpproductcat= new Select
-	 * (driver.findElement(By.name("data[Bug][product_category]")));
-	 * drpproductcat.selectByVisibleText("Software");
-	 * driver.findElement(By.id("BugDescription")).
-	 * sendKeys("Hello this is for testing");
-	 * driver.findElement(By.id("BugBuildNumber")).sendKeys("A001");
-	 * driver.findElement(By.id("BugHowOffen")).sendKeys("Daily");
-	 * driver.findElement(By.id("BugHasOccured")).sendKeys("No"); WebElement
-	 * Bugroot= driver.findElement(By.xpath(
-	 * "//div[3]/div[3]/div/div[2]/div/div/div/div/form/div[2]/div[9]/div[1]/label"
-	 * )); Bugroot.click(); // click the "Attachment" button
-	 * driver.findElement(By.name("data[Bug][attachment]")).click();
-	 * StringSelection ss = new
-	 * StringSelection("C:\\Users\\Anuj Singh\\Desktop\\aa.txt");
-	 * Toolkit.getDefaultToolkit().getSystemClipboard().setContents(ss, null);
-	 * 
-	 * //imitate mouse events like ENTER, CTRL+C, CTRL+V Robot robot = new
-	 * Robot(); robot.keyPress(KeyEvent.VK_ENTER);
-	 * robot.keyRelease(KeyEvent.VK_ENTER); robot.keyPress(KeyEvent.VK_CONTROL);
-	 * robot.keyPress(KeyEvent.VK_V); robot.keyRelease(KeyEvent.VK_V);
-	 * robot.keyRelease(KeyEvent.VK_CONTROL); robot.keyPress(KeyEvent.VK_ENTER);
-	 * robot.keyRelease(KeyEvent.VK_ENTER); Thread.sleep(3000);
-	 * driver.findElement(By.xpath(
-	 * "//div[3]/div[3]/div/div[2]/div/div/div/div/form/div[2]/div[11]/button"))
-	 * .submit();
-	 * 
-	 * }
-	 */
+	@Test(priority = 3)
+
+	public void Hyve_Care_Report_a_Bug() throws AWTException, InterruptedException {
+		driver.get(hyvecare);
+		driver.findElement(By.xpath("//div[2]/div[3]/div/div/div/div[2]/div[2]/a/div/div[1]/img")).click();;
+		driver.findElement(By.xpath("//div[2]/div[3]/div/div/ul/li[2]/a")).click();
+		// Enter Values in Text Field
+		driver.findElement(By.id("BugName")).sendKeys("Anuj Singh");
+		driver.findElement(By.id("BugEmail")).sendKeys("anuj@gmail.com");
+		// Select Device from Drop down.
+		Select drpproductid = new Select(driver.findElement(By.name("data[Bug][product_id]")));
+		drpproductid.selectByVisibleText("Buzz BU01"); // Select Product Category from Drop down.
+		Select drpproductcat = new Select(driver.findElement(By.name("data[Bug][product_category]")));
+		drpproductcat.selectByVisibleText("Software");
+		driver.findElement(By.id("BugDescription")).sendKeys("Hello this is for testing");
+		driver.findElement(By.id("BugBuildNumber")).sendKeys("A001");
+		driver.findElement(By.id("BugHowOffen")).sendKeys("Daily");
+		driver.findElement(By.id("BugHasOccured")).sendKeys("No");
+		WebElement Bugroot = driver.findElement(By.xpath("//div[2]/div[3]/div/div/div[2]/div/div/div/div/form/div[2]/div[9]/div[1]/label"));
+		Bugroot.click(); // click the "Attachment" button
+		driver.findElement(By.name("data[Bug][attachment]")).click();
+		StringSelection ss = new StringSelection("C:\\Users\\Anuj Singh\\Desktop\\aa.txt");
+		Toolkit.getDefaultToolkit().getSystemClipboard().setContents(ss, null);
+
+		// imitate mouse events like ENTER, CTRL+C, CTRL+V
+		Robot robot = new Robot();
+		robot.keyPress(KeyEvent.VK_ENTER);
+		robot.keyRelease(KeyEvent.VK_ENTER);
+		robot.keyPress(KeyEvent.VK_CONTROL);
+		robot.keyPress(KeyEvent.VK_V);
+		robot.keyRelease(KeyEvent.VK_V);
+		robot.keyRelease(KeyEvent.VK_CONTROL);
+		Thread.sleep(5000);
+		robot.keyPress(KeyEvent.VK_ENTER);
+		robot.keyRelease(KeyEvent.VK_ENTER);
+		//Thread.sleep(3000);
+		driver.findElement(By.xpath("//div[2]/div[3]/div/div/div[2]/div/div/div/div/form/div[2]/div[11]/button")).submit();
+
+	}
 
 	@AfterTest
 	public void tearDown() throws Exception {
