11f53e1775d1e3c6e4dd2f01e598ad8b9a1db375
Anuj Singh <Anuj Singh@LAPTOP-IL45AT31>
2016-12-28 17:42:14 +0530

    ok

--- hyve-qa-auto/src/com/hyve/automationFramework/TestNG_automationTC.java
+++ hyve-qa-auto/src/com/hyve/automationFramework/TestNG_automationTC.java
@@ -14,7 +14,7 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
 
 import com.hyve.pageObjects.Home_Page;
-
+import com.hyve.pageObjects.productHyve;
 import com.hyve.utility.Constant;
 
 import com.hyve.utility.ExcelUtils;
@@ -28,9 +28,9 @@ public class TestNG_automationTC {
 
 	ExtentReports report;
 	ExtentTest logger;
-	
+
 	private static WebDriver driver = null;
-	
+
 	// public static void main(String[] args) throws Exception {
 
 	// This is to open the Excel file. Excel path, file name and the sheet name
@@ -38,7 +38,8 @@ public class TestNG_automationTC {
 	@Test(priority = 1)
 	public void testLogin() throws Exception {
 
-		report = new ExtentReports("F:\\Automation\\git-codebase\\hyve-qa-automation\\hyve-qa-auto\\Automation_Report.html");
+		report = new ExtentReports(
+				"F:\\Automation\\git-codebase\\hyve-qa-automation\\hyve-qa-auto\\Automation_Report.html");
 		logger = report.startTest("testLogin");
 
 		ExcelUtils.setExcelFile(Constant.Path_TestData + Constant.File_TestData, "Sheet1");
@@ -61,7 +62,7 @@ public class TestNG_automationTC {
 		Assert.assertEquals(actualtext, expected);
 		logger.log(LogStatus.PASS, " Login Successfully and User Verified ");
 
-		System.out.println("Login Successfully and User Verified, now it is the time to Log Off buddy.");
+		System.out.println("Login Successfully and User Verified, now it is the time to Add Product buddy.");
 
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
@@ -70,12 +71,47 @@ public class TestNG_automationTC {
 	}
 
 	@Test(priority = 2)
-	public void testLogout() throws Exception {
+	public void testCart() throws InterruptedException {
+		logger = report.startTest("testCart");
+		driver.get(Constant.URL);
+		logger.log(LogStatus.INFO, " Browser up and go to Pryme Page ");
+		driver.findElement(By.xpath("//a[@href='/pryme']")).click();
+		productHyve.Buy_pryme(driver).click();
+		logger.log(LogStatus.INFO, " Add Pryme into Cart Successfully");
 		
+		//driver.findElement(By.xpath("//div[2]/div[1]/div/div/div[2]/div[1]/ul/li[11]/a")).click();
+		
+		String subtotal=driver.findElement(By.xpath("//*[@id='CartForm']/table/tbody/tr/td[4]/div")).getText();
+		System.out.println("subtotal = "+ subtotal);
+		
+		String saleprice=driver.findElement(By.xpath("//*[@id='CartForm']/table/tbody/tr/td[3]/div")).getText();
+		System.out.println("Sale Price = "+ saleprice);
+		
+		String quantity=driver.findElement(By.xpath("//*[contains(@name,'qty')]")).getAttribute("value");
+		System.out.println("Quantity = "+ quantity);
+		
+	
+	double isubtotal= Double.parseDouble(subtotal.replaceAll("[^0-9\\.]+", ""));
+	//System.out.println("isubtotal is "+isubtotal);
+	double isaleprice= Double.parseDouble(saleprice.replaceAll("[^0-9\\.]+", ""));
+	//System.out.println("isaleprice is "+isaleprice);
+		int iquantity=Integer.parseInt(quantity);
+		double expectedsubtotal=(isaleprice*iquantity);
+		System.out.println("Expected Subtotal is "+expectedsubtotal);
+		Assert.assertEquals(isubtotal, expectedsubtotal);
+		System.out.println("Great.. Subtotal is correct");
+		logger.log(LogStatus.PASS, " Added Product quantity and price verified Successfully");
+				
+		Thread.sleep(3000);
+	}
+
+	@Test(priority = 3)
+	public void testLogout() throws Exception {
+
 		logger = report.startTest("testLogout");
 
 		Home_Page.lnk_LogOut(driver).click();
-		
+
 		logger.log(LogStatus.INFO, " Logout Successfully ");
 
 		// This is to send the PASS value to the Excel sheet in the result
@@ -93,7 +129,7 @@ public class TestNG_automationTC {
 		}
 		report.endTest(logger);
 		report.flush();
-		//driver.get("F:\\Automation\\git-codebase\\hyve-qa-automation\\hyve-qa-auto\\Automation_Report.html");
+		// driver.get("F:\\Automation\\git-codebase\\hyve-qa-automation\\hyve-qa-auto\\Automation_Report.html");
 		// driver.quit();
 	}
 
---
+++ hyve-qa-auto/src/com/hyve/pageObjects/productHyve.java
@@ -0,0 +1,23 @@
+package com.hyve.pageObjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class productHyve {
+	private static WebElement element = null;
+	
+	public static WebElement Buy_pryme(WebDriver driver){
+		element = driver.findElement(By.xpath("//a[@id='149']"));
+		return element;
+	}
+	public static WebElement Buy_buzz(WebDriver driver){
+		element = driver.findElement(By.xpath("//a[@id='2']"));
+		return element;
+	}
+	public static WebElement Buy_storm(WebDriver driver){
+		element = driver.findElement(By.xpath("//a[@id='1']"));
+		return element;
+	}
+
+}
